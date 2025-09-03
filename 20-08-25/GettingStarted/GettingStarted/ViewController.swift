749c4895d9694a9bc0993db2418ee13f27d06bd8
Facultad Contaduría y Administración <facultadcontaduriayadministracion@MacBook-13.local>
2025-09-03 11:18:19 -0600

    Concatenacion

--- 20-08-25/GettingStarted/GettingStarted/ViewController.swift
+++ 20-08-25/GettingStarted/GettingStarted/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lbltext: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
     }
 
-
+    
+    @IBAction func didTap(_ sender: Any) {
+        lbltext.text! += "Hice un tap en un iPhone"
+    }
 }
 
